6d76eff6d171ee836f7d95eb9a545fda6141f8c2
ofekdeitch-oligo <121801386+ofekdeitch-oligo@users.noreply.github.com>
2023-08-11 12:29:47 +0300

    impl

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { MapPage } from './pages/map-page';
 import { Route, Routes } from 'react-router-dom';
+import { LandingPage } from './pages/landing-page';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="" element={<MapPage />} />
+        <Route path="map" element={<MapPage />} />
+        <Route path="" element={<LandingPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/landing-page/index.tsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import styled from "styled-components";
+import { Section } from './Section';
+import icon from './icon.png';
+
+interface Props { }
+
+export const LandingPage: React.FC<Props> = (props: Props) => {
+  return (
+    <>
+      <Section backgroundColor='#168eff'>
+        <Gap height={20} />
+        <center>
+          <img src={icon} width="200px" alt='' />
+        </center>
+        <Title>דו-גלגל</Title>
+        <Subtitle>רוכבים יחד אל עבר עיר בטוחה יותר</Subtitle>
+      </Section>
+      <Section>
+        <SectionTitle>איך זה עובד?</SectionTitle>
+        <Step>
+          <StepLine />
+          <Number>1.</Number>
+          <StepText>
+            האפליקציה של <strong>דו-גלגל</strong> מאפשרת לרוכבי אופניים וקורקינט לדווח בזמן אמת על השבילים שבהם הם רוכבים.
+          </StepText>
+        </Step>
+        <Step>
+          <StepLine />
+          <Number>2.</Number>
+          <StepText> את המידע הזה אנחנו אוספים ומנגישים לכם באמצעות מפת-חום. שבילים שבשימוש תדיר צבועים באדום, ואילו שבילים בשימוש פחות תדיר צבועים בירוק.</StepText>
+        </Step>
+        <Step >
+          <Number>3.</Number>
+          <StepText>לבסוף, המידע שאספנו ביחד מוצג למתכנני העיר, ואיתו אנחנו קובעים היכן יש לבנות שבילים חדשים, והיכן כדאי לשפר את השבילים הקיימים.</StepText>
+        </Step>
+      </Section >
+    </>
+  );
+}
+
+
+const Title = styled.h1`
+  font-size: 56px;
+  font-weight: 600;
+  color: #fff;
+  margin: 0;
+  text-align: center;
+`
+
+const Subtitle = styled.h2`
+  font-size: 20px;
+  font-weight: 200;
+  color: #fff;
+  text-align: center; 
+`
+
+const Gap: React.FC<{ height: number }> = styled.div`
+  height: ${(props: any) => props.height}px;
+`
+
+const SectionTitle = styled.h2`
+  font-size: 24px;
+  font-weight: 600;
+  text-align: center;
+`
+
+
+const Step = styled.div`
+  display: flex;
+  align-items: flex-start;
+  position: relative;
+  padding-bottom: 36px;
+`
+
+const StepText = styled.p`
+  font-weight: 200;
+  font-size: 18px;
+  line-height: 1.5;
+  color: #525252;
+  padding-top: 12px;
+
+  width: 60vw;
+  margin: 0 auto;
+
+  flex: 1;
+  
+`
+
+const Number = styled.span`
+  font-size: 36px;
+  color: #1077d7;
+  font-weight: 600;
+  width: 1.5ch;
+  margin-left: 24px;
+  line-height: 1;
+  background: #fff;
+  display: inline-block;
+  padding: 8px 0;
+  z-index: 2;
+  text-align: center;
+`
+
+const StepLine = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  background: #b5dbff;
+  width: 3px;
+`
